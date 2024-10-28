59e4120b2fb3858e7b678a7f838cdb6ede19ccf9
nahid-priom <nahid.priom.06@gmail.com>
2024-10-28 13:48:32 +0600

    index file is updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import "./index.css";
 
 
-const container = document.getElementById('root'); 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
