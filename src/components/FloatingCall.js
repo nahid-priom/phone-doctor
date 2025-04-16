dd2e36f4a4a481d6c5cc0d805d2982884c488fe9
nahid-priom <nahid.priom.06@gmail.com>
2025-04-16 21:55:00 +0600

    call now button is updated

--- src/components/FloatingCall.js
+++ src/components/FloatingCall.js
@@ -4,10 +4,10 @@ const FloatingCallButton = () => {
   return (
     <a
       href="tel:+12406965671"
-      className="fixed bottom-9 animate-bounce right-5 bg-red-600 text-white p-2 rounded-full shadow-lg hover:bg-red-600 transition duration-300"
+      className="fixed bottom-9 animate-bounce right-5 bg-blue-600 text-white p-2 rounded-xl shadow-lg hover:bg-blue-900 transition duration-300"
       aria-label="Call Us"
     >
-      <img width="40" height="40" className='bg-white font-bold rounded-full p-2' src="https://img.icons8.com/ios/50/phone--v1.png" alt="phone--v1"/>
+      <img width="30" height="30" className='bg-white font-bold rounded-xl p-2' src="https://img.icons8.com/ios/50/phone--v1.png" alt="phone--v1"/>
     </a>
   );
 };
